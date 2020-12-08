7a9db2df54d01d88679d02b8d74559b77f51d9f9
Gerardo Vázquez <herr.ardo@gmail.com>
2020-12-09 00:47:51 +0100

    Set github pages deploy and github actions

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Deployed by Github Actions
         </a>
       </header>
     </div>
