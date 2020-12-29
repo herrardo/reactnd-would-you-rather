92921fb3cbb32c5dfb461ab740854f27464a65b4
Gerardo Vázquez <herr.ardo@gmail.com>
2020-12-29 22:42:58 +0100

    Style leaderboard

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { orderUsers } from '../selectors';
+import { Avatar } from '@material-ui/core';
 
 const Leaderboard = ({ orderedUsers }) => {
   return (
-    <div className='users'>
-      <h3 className='center'>List of questions</h3>
-      <ul className='dashboard-list'>
+    <div className='leaderboard'>
+      <h3 className='center'>Leaderboard</h3>
+      <ul className='leaderboard-list'>
         {Object.values(orderedUsers).map(user => (
-          <li key={user.id}>
-            <h1>{user.name}</h1>
-            <h3>{user.interactions}</h3>
+          <li className='leaderboard-list__item' key={user.id}>
+            <h3>{user.name}</h3>
+            <Avatar src={user.avatarURL} className='avatar' />
+            <div className='leaderboard-list__result'>
+              Total: <span className='total'>{user.interactions.total}</span>
+            </div>
+            <div className='leaderboard-list__result'>
+              Number of questions: {user.interactions.questions}
+            </div>
+            <div className='leaderboard-list__result'>
+              Number of answers: {user.interactions.answers}
+            </div>
           </li>
         ))}
       </ul>
